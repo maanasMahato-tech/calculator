7b78b4c17feda0eec2f351183e11c91a8fbbe92b
maanas mahato <maanasmahato.work.acc@gmail.com>
2021-06-05 00:20:00 +0545

    v1

--- calculator.cpp
+++ calculator.cpp
@@ -1,17 +1,61 @@
 #include <iostream>
 
-
 using namespace std;
 
-int main(){
-    int n, s, sum = 0;
-    cout << "+ - * /" << endl;
-    for(int i = 0; i < 10000; i++){
-        cin >> s >> n;
-        if(s == '+'){
-            sum = sum + n;
+int main()
+{
+    int a, sum = 0;
+    char n;
+
+    for (int i = 0; i < 100000; i++)
+    {
+        cout << "enter the signs to make the sum of the numbers: ";
+        cin >> n;
+
+        if (n == '+')
+        {
+            cout << "enter the number you want to make sum of: ";
+            cin >> a;
+            sum = sum + a;
             cout << sum << endl;
         }
+        else if (n == '-')
+        {
+            cout << "enter the number you want to make sum of: ";
+            cin >> a;
+            sum = sum - a;
+            cout << sum << endl;
+        }
+        else if (n == '*')
+        {
+            cout << "enter the number you want to make sum of: ";
+            cin >> a;
+            sum = sum * a;
+            cout << sum << endl;
+        }
+        else if (n == '/')
+        {
+            cout << "enter the number you want to make sum of: ";
+            cin >> a;
+            sum = sum / a;
+            cout << sum << endl;
+        }
+        else if (n == 'c')
+        {
+            sum = 0;
+            cout << sum << endl;
+            break;
+        }
+        else if (n == '=')
+        {
+            cout << sum << endl;
+            break;
+        }
+        else
+        {
+            cout << sum << endl;
+            cout << "error" << endl;
+            break;
+        }
     }
-    
 }
